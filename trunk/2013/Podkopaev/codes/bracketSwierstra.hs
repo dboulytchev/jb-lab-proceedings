b8d225c7a5f2a7449a201bf6824ffaef0f06124f
dboulytchev@gmail.com <dboulytchev@gmail.com@93afa984-d415-ad35-f4b9-c670ea483997>
2013-10-16 17:47:46 +0000

    Switched to a5 pagesize

--- trunk/2013/Podkopaev/codes/bracketSwierstra.hs
+++ trunk/2013/Podkopaev/codes/bracketSwierstra.hs
@@ -1,5 +1,6 @@
 docFromOperation (Write exp) =
-	hor1_vert ((text "write(") <> (docFromExpression exp)) (text ")")
+	hor1_vert ((text "write(") <> (docFromExpression exp)) 
+                   (text ")")
 
 hor1_vert :: Doc -> Doc -> Doc
 hor1_vert a b = (a $$ b) <|> (element_h1 (a <> b))
